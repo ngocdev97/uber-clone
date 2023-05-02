129aba67574e633850f2167ce9fed63da854a343
ngocnghien97 <ngoc1997307@gmail.com>
2023-05-03 01:20:30 +0700

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Ngọc Lê on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
